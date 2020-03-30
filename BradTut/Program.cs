38a78614c710e96a145c43b60f1e61fdb114cc85
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-03-30 22:41:15 +0200

    refactor

--- BradTut/Program.cs
+++ BradTut/Program.cs
@@ -8,6 +8,66 @@ namespace BradTut
         {
             while (true)
             {
+                GetGameInfo();
+
+                GetUserInfo();
+                //Initialize a correct number
+                //int correctNumber = 7;
+                int correctNumber = RandomNumber();
+
+                //intialize a guess
+                int guess = 0;
+
+                //Ask user for a number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                //While guess is not correct
+                while(guess != correctNumber)
+                {
+                //Get user input
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+
+                        GameColor(ConsoleColor.Red, "Please enter an actual number");
+
+                        //Keep going
+                        continue;
+                    }
+
+                    //Cast it to int and put it in guess
+                    guess = Int32.Parse(input);
+
+                    //Match guess to the correct number
+                    if(guess != correctNumber)
+                    {
+                            GameColor(ConsoleColor.Red, "Wrong Number, Please try again");
+                    }
+                }
+
+                    GameColor(ConsoleColor.DarkYellow, "You are CORRECT!!!");
+
+                    //Ask to play again
+                    Console.WriteLine("Play Again [Y or N]");
+
+                    string answer = Console.ReadLine().ToUpper();
+
+                    if(answer == "Y")
+                    {
+                        continue;
+                    }
+                    else if(answer == "N")
+                    {
+                        return;
+                    }
+
+            }
+
+        }
+
+        static void GetGameInfo()
+        {
             //Declare game Variables
             string appName = "Number Guesser";
             string appVersion = "Version 1.0.0";
@@ -21,94 +81,35 @@ namespace BradTut
 
             // Reset Text color
             Console.ResetColor();
+        }
 
+        static void GetUserInfo()
+        {
             //Ask for a user name
             Console.WriteLine("What is your name?");
 
             //Get User Input
-            string input;
-
-            input = Console.ReadLine();
+            string input = Console.ReadLine();
 
             // Welcome message
             Console.WriteLine(" Hello {0}, Lets Play a game....", input);
 
-            //Initialize a correct number
-            //int correctNumber = 7;
+        }
+        static void GameColor(ConsoleColor color, string message)
+        {
+            //Change text color
+            Console.ForegroundColor = color;
+
+            //Tell User its the wrong number
+            Console.WriteLine(message);
+
+            // Reset Text color
+            Console.ResetColor();
+        }
+        static int RandomNumber()
+        {
             Random random = new Random();
-            int correctNumber = random.Next(1, 11);
-
-            //intialize a guess
-            int guess;
-
-            guess = 0;
-
-            //Ask user for a number
-            Console.WriteLine("Guess a number between 1 and 10");
-
-            //While guess is not correct
-            while(guess != correctNumber)
-            {
-                //Get user input
-                input = Console.ReadLine();
-
-                if (!int.TryParse(input, out guess))
-                {
-                    //Change text color
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    //Tell User its not a number
-                    Console.WriteLine("Please enter an actual number");
-
-                    // Reset Text color
-                    Console.ResetColor();
-
-                    //Keep going
-                    continue;
-                }
-
-                //Cast it to int and put it in guess
-                guess = Int32.Parse(input);
-
-                //Match guess to the correct number
-                if(guess != correctNumber)
-                {
-                    //Change text color
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    //Tell User its the wrong number
-                    Console.WriteLine("Wrong Number, please try again");
-
-                    // Reset Text color
-                    Console.ResetColor();
-                }
-            }
-
-                //Change text color
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-                //Tell User its the right number
-                Console.WriteLine("You are CORRECT!!!");
-
-                // Reset Text color
-                Console.ResetColor();
-
-                //Ask to play again
-                Console.WriteLine("Play Again [Y or N]");
-
-                string answer = Console.ReadLine().ToUpper();
-
-                if(answer == "Y")
-                {
-                    continue;
-                }
-                else if(answer == "N")
-                {
-                    return;
-                }
-
-            }
-
+            return random.Next(1, 11);
         }
     }
 }
